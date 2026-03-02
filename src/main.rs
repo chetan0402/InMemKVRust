a29be3ccf7c6b6e4b0f15d4dfba1d9db258e8a39
Chetan0402 <chetansharma67g@gmail.com>
2026-03-02 21:20:48 +0530

    reads from tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
+
+fn handle_connection(stream: TcpStream){
+    let mut reader = BufReader::new(stream);
+    let mut buffer = String::new();
+
+    match reader.read_line(&mut buffer) {
+        Ok(_) => {},
+        Err(err) => {println!("{}",err)},
+    };
+
+    println!("{}",buffer)
+}
+
+fn main(){
+    let listener = TcpListener::bind("localhost:9876").unwrap();
+
+    for stream in listener.incoming(){
+        match stream {
+            Ok(stream) => {handle_connection(stream);},
+            Err(error) => println!("{}",error),
+        }
+    }
 }
