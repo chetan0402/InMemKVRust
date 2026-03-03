cbc62f66ac76b13391c299e925256e612b2025f6
Chetan0402 <chetansharma67g@gmail.com>
2026-03-03 12:12:54 +0530

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,59 @@
-use std::{cell::RefCell, collections::HashMap, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 thread_local! {
     static MAP: RefCell<HashMap<String,String>> = RefCell::new(HashMap::new());
 }
 
-fn handle_connection(stream: TcpStream){
+fn handle_connection(stream: TcpStream) -> Result<(), String> {
     let mut reader = BufReader::new(stream);
     let mut buffer = String::new();
 
-    match reader.read_line(&mut buffer) {
-        Ok(_) => {},
-        Err(err) => {println!("{}",err)},
-    };
+    reader
+        .read_line(&mut buffer)
+        .map_err(|err| format!("read err: {}", err))?;
 
-    buffer=buffer.trim_end().to_string();
-    println!("{}",buffer);
-    let tokens: Vec<&str> = buffer.split(" ").collect();
-    if tokens[0]=="SET"{
-        MAP.with(|m|{
-            m.borrow_mut().insert(tokens[1].to_string(), tokens[2].to_string());
-            println!("Insert@{}:{}",tokens[1].to_string(),tokens[2].to_string());
-        });
-    }
-    if tokens[0]=="GET"{
-        let mut value: String = String::new();
-        MAP.with(|m|{
-            match m.borrow().get(&tokens[1].to_string()){
-                None => {println!("Key({}) not present",tokens[1].to_string())},
-                Some(v) => {value=v.clone()}
+    print!("{}", buffer);
+    let mut iter = buffer.split_whitespace();
+    match iter.next() {
+        Some("SET") => {
+            let key = iter.next().ok_or("invalid command")?.to_string();
+            let value = iter.next().ok_or("invalid command")?.to_string();
+            MAP.with(|m| {
+                m.borrow_mut().insert(key, value);
+            });
+        }
+        Some("GET") => {
+            let value = {
+                let key = iter.next().ok_or("invalid command")?.to_string();
+                MAP.with(|m| m.borrow().get(&key).cloned())
             };
 
-        });
-        if reader.get_mut().write(value.as_bytes()).is_err(){
-            println!("Response failed");
+            reader
+                .get_mut()
+                .write(value.ok_or("key not found")?.as_bytes())
+                .map_err(|err| format!("write err:{}", err))?;
         }
+        Some(command) => return Err(format!("unknown command: {}", command)),
+        None => return Err("No command found".to_string()),
     }
+
+    Ok(())
 }
 
-fn main(){
+fn main() {
     let listener = TcpListener::bind("localhost:9876").unwrap();
 
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {handle_connection(stream);},
-            Err(error) => println!("{}",error),
+            Ok(stream) => {
+                handle_connection(stream).unwrap_or_else(|e| eprintln!("{}",e))
+            }
+            Err(error) => println!("{}", error),
         }
     }
 }
