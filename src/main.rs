0ca01184095d57fa4f87de07cfbc816ec70613e6
Chetan0402 <chetansharma67g@gmail.com>
2026-03-24 22:20:42 +0530

    seperate processing command from handling connection

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,8 @@ thread_local! {
     static MAP: RefCell<HashMap<String,String>> = RefCell::new(HashMap::new());
 }
 
-fn handle_connection(stream: TcpStream, wal: &mut File) -> Result<(), String> {
-    let mut reader = BufReader::new(stream);
-    let mut buffer = String::new();
-
-    reader
-        .read_line(&mut buffer)
-        .map_err(|err| format!("read err: {}", err))?;
-
-    wal.write_all(buffer.as_bytes())
-        .map_err(|err| format!("wal err: {}", err))?;
-    let mut iter = buffer.split_whitespace();
+fn process_command(command: &String) -> Result<String, String> {
+    let mut iter = command.split_whitespace();
     match iter.next() {
         Some("SET") => {
             let key = iter.next().ok_or("invalid command")?.to_string();
@@ -35,20 +26,40 @@ fn handle_connection(stream: TcpStream, wal: &mut File) -> Result<(), String> {
                 MAP.with(|m| m.borrow().get(&key).cloned())
             };
 
-            reader
-                .get_mut()
-                .write(value.ok_or("key not found")?.as_bytes())
-                .map_err(|err| format!("write err:{}", err))?;
+            return value.ok_or(String::from("key not found"));
         }
         Some(command) => return Err(format!("unknown command: {}", command)),
         None => return Err("No command found".to_string()),
     }
 
+    Ok(String::from(""))
+}
+
+fn handle_connection(stream: TcpStream, wal: &mut File) -> Result<(), String> {
+    let mut reader = BufReader::new(stream);
+    let mut buffer = String::new();
+
+    reader
+        .read_line(&mut buffer)
+        .map_err(|err| format!("read err: {}", err))?;
+
+    wal.write_all(buffer.as_bytes())
+        .map_err(|err| format!("wal err: {}", err))?;
+
+    reader
+        .get_mut()
+        .write(process_command(&buffer)?.as_bytes())
+        .map_err(|err| err.to_string())?;
+
     Ok(())
 }
 
 fn main() {
-    let mut wal = OpenOptions::new().write(true).create_new(true).open("WAL.log").unwrap();
+    let mut wal = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open("WAL.log")
+        .unwrap();
     let listener = TcpListener::bind("localhost:9876").unwrap();
 
     for stream in listener.incoming() {
